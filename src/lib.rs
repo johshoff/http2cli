92ead5427d90420e27a436c62c40af5d8a778610
Johannes Hoff <johshoff@gmail.com>
2015-07-02 21:50:12 -0700

    Upgrade connection, except settings header

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
 use std::io::Write;
 use std::io::Result;
 
-pub struct Header; // TODO: Flesh out
+//const HTTP_2_PREFACE : &'static [u8] = b"PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n";
 
-pub struct Request {
+pub struct Header<'a> {
+	pub key:   &'a str,
+	pub value: &'a str,
+}
+
+pub struct Request<'a> {
     pub verb     : Verb,
-    pub resource : String,
-    pub body     : Option<String>,
-    pub headers  : Vec<Header>,
+    pub resource : &'a str,
+    pub body     : Option<&'a str>,
+    pub headers  : Vec<Header<'a>>,
+    pub host     : &'a str,
 }
 
 #[derive(Debug)]
@@ -28,12 +34,27 @@ pub enum Verb {
 pub fn request_line_1_1(request: &Request, out: &mut Write) -> Result<usize> {
     // TODO: This function assumes that the write calls write the entire buffer passed
     //       to it. This is not the case, and should be handeled properly.
+	// TODO: Encode resource
 
     let bytes_written =
         try!(out.write(format!("{:?}", request.verb).as_bytes())) +
         try!(out.write(b" ")) +
-        try!(out.write(&*request.resource.as_bytes())) +
-        try!(out.write(b" HTTP/1.1"));
+        try!(out.write(request.resource.as_bytes())) +
+        try!(out.write(b" HTTP/1.1\n"));
+
+    Ok(bytes_written)
+}
+
+pub fn header_field_1_1(header: &Header, out: &mut Write) -> Result<usize> {
+    // TODO: This function assumes that the write calls write the entire buffer passed
+    //       to it. This is not the case, and should be handeled properly.
+	// TODO: Encode headers
+
+    let bytes_written =
+        try!(out.write(header.key.as_bytes())) +
+        try!(out.write(b": ")) +
+        try!(out.write(header.value.as_bytes())) +
+        try!(out.write(b"\n"));
 
     Ok(bytes_written)
 }
@@ -45,39 +66,55 @@ pub fn request_try_upgrade(request: &Request, out: &mut Write) -> Result<usize> 
     // TODO: This function assumes that the write calls write the entire buffer passed
     //       to it. This is not the case, and should be handeled properly.
 
-    let bytes_written =
-        try!(request_line_1_1(request, out));
+    let mut bytes_written =
+        try!(request_line_1_1(request, out)) +
+        try!(header_field_1_1(&Header { key: "Host", value: request.host }, out)) +
+        try!(header_field_1_1(&Header { key: "Connection", value: "Upgrade, HTTP2-Settings" }, out)) +
+        try!(header_field_1_1(&Header { key: "Upgrade", value: "h2c" }, out)) +
+        try!(header_field_1_1(&Header { key: "HTTP2-Settings", value: "???" }, out)) +
+        try!(out.write(b"\n"));
 
-    // TODO: Other headers
+	for header in &request.headers {
+		bytes_written += try!(header_field_1_1(header, out));
+	}
 
     Ok(bytes_written)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{ request_line_1_1, Verb, Request };
-
-    /// Shim while waiting for String::from_str to stabilize or be replaced
-    fn create_string(slice: &str) -> String {
-        let mut string = String::new();
-        string.push_str(slice);
-
-        string
-    }
+    use super::{ request_line_1_1, request_try_upgrade, Verb, Request };
 
     #[test]
     fn test_request_line_1_1() {
         let mut out = Vec::new();
 
         let request = Request { verb:     Verb::GET,
-                                resource: create_string("/index.html"),
+                                resource: "/index.html",
                                 body:     None,
                                 headers:  vec![],
+                                host:     "example.com",
                                 };
 
         request_line_1_1(&request, &mut out).unwrap();
 
-        assert_eq!(String::from_utf8(out).unwrap(), "GET /index.html HTTP/1.1");
+        assert_eq!(String::from_utf8(out).unwrap(), "GET /index.html HTTP/1.1\n");
+    }
+
+	#[test]
+    fn test_request_try_upgrade() {
+        let mut out = Vec::new();
+
+        let request = Request { verb:     Verb::GET,
+                                resource: "/index.html",
+                                body:     None,
+                                headers:  vec![],
+                                host:     "example.com",
+                                };
+
+        request_try_upgrade(&request, &mut out).unwrap();
+
+        assert_eq!(String::from_utf8(out).unwrap(), "GET /index.html HTTP/1.1\nHost: example.com\nConnection: Upgrade, HTTP2-Settings\nUpgrade: h2c\nHTTP2-Settings: ...\n\n");
     }
 
 }
